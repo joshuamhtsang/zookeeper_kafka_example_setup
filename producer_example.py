19730c7a5269b428508c9ac4f463689eea779308
joshuamhtsang <joshua.mh.tsang@gmail.com>
2020-01-12 18:30:21 +0000

    add req.txt file, also added producer script

--- producer_example.py
+++ producer_example.py
@@ -1,7 +1,15 @@
 from kafka import KafkaProducer
+import json
 
 if __name__ == '__main__':
-    producer = KafkaProducer(bootstrap_servers='127.0.0.1:32181', acks=1,
-                             value_serializer=lambda m: json.dumps(m).encode('ascii'),
-                             retries=2, max_request_size=2 * 1024 * 1024,
-                             request_timeout_ms=3000)
+    # Make KafkaProducer object.
+    producer = KafkaProducer(
+        bootstrap_servers='127.0.0.1:9092',
+        acks=1,
+        value_serializer=lambda m: json.dumps(m).encode('ascii'),
+        retries=2,
+        max_request_size=2*1024*1024,
+        request_timeout_ms=3000
+    )
+
+    producer.send('JOSH_TOPIC_1', value={'event': 'click'})
