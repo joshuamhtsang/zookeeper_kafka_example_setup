27d63719961605c678263e046f508e66696c8e85
joshuamhtsang <joshua.mh.tsang@gmail.com>
2020-01-19 23:37:25 +0000

    added farewell topic and agent action

--- faust_example.py
+++ faust_example.py
@@ -1,4 +1,5 @@
 import faust
+import string
 
 app = faust.App(
     'faust-example',
@@ -7,9 +8,16 @@ app = faust.App(
 )
 
 greetings_topic = app.topic('greetings')
+farewells_topic = app.topic('farewells')
 
 
 @app.agent(greetings_topic)
 async def greet(greetings):
     async for greeting in greetings:
         print(greeting)
+
+
+@app.agent(farewells_topic)
+async def farewell(farewells):
+    async for farewell in farewells:
+        print(str(farewell).upper())
